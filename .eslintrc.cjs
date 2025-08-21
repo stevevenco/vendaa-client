e2e612f2fb0b151b2b44fb5517ede8372b5d6d24
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2025-08-21 10:37:41 +0100

    convert to react

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+// .eslintrc.cjs
+module.exports = {
+  // ... other config
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier', // Add this
+  ],
+  // ... other config
+}
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+// src/App.tsx
+function App() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-gray-900">
+      <h1 className="text-3xl font-bold text-white underline">
+        Hello, Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
